be0de15cf682ce8369c80524f082fd2893c122fd
Graham Rhodes <1964861+grahamar@users.noreply.github.com>
2019-12-29 22:49:31 +0000

    fix

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box } from 'rebass'
-import GetInTouch from '../GetInTouch'
 import { Logo, HiLine, BuildLine, Company } from './styles'
 
 export default class Hello extends React.Component {
@@ -11,23 +10,12 @@ export default class Hello extends React.Component {
         <Logo>Graham Rhodes.</Logo>
         <BuildLine>I build software and teams to solve problems.</BuildLine>
         <p>
-          I&#39;m a software engineer and technical lead based in Dublin,
-          Ireland specialising
+          I&#39;m a software engineer and technical lead based in Ireland specialising
           <br />
           in building and designing high-quality, scalable software solutions.
           <br />
           From containers and CI/CD to RESTful APIs and JavaScript UIs.
-          <br />
-          Currently working full-time for{' '}
-          <Company
-            target="_blank"
-            href="https://aylien.com/"
-            rel="noopener noreferrer"
-          >
-            Aylien
-          </Company>
         </p>
-        <GetInTouch />
       </Box>
     )
   }
